8945b51372e04a2a2bf50f252409f73beab6fa15
yamatchan <yamatchan_yy@yahoo.co.jp>
2015-11-18 22:09:52 +0900

    ARP Requestの際にもARP Lookup Tableを更新するように変更

--- lib/simple_router.rb
+++ lib/simple_router.rb
@@ -38,6 +38,7 @@ class SimpleRouter < Trema::Controller
     case message.data
     when Arp::Request
       logger.info "Arp::Request"
+      add_arp_lookup_flow_entry(dpid, message)
     when Arp::Reply
       logger.info "Arp::Reply"
       add_arp_lookup_flow_entry(dpid, message)
@@ -59,8 +60,9 @@ class SimpleRouter < Trema::Controller
   end
 
   def packet_in_arp_reply(dpid, message)
-    flush_unsent_packets(dpid,
-                         message.data.sender_protocol_address,
+    flush_unsent_packets(
+      dpid,
+      message.data.sender_protocol_address,
     )
   end
 
@@ -107,29 +109,37 @@ class SimpleRouter < Trema::Controller
 
   def send_arp_request(dpid, destination_ip, interface)
     arp_request =
-      Arp::Request.new(source_mac: interface.mac_address,
-                       sender_protocol_address: interface.ip_address,
-                       target_protocol_address: destination_ip)
-    send_packet_out(dpid,
-                    raw_data: arp_request.to_binary,
-                    actions: SendOutPort.new(interface.port_number))
+      Arp::Request.new(
+        source_mac: interface.mac_address,
+        sender_protocol_address: interface.ip_address,
+        target_protocol_address: destination_ip,
+      )
+    send_packet_out(
+      dpid,
+      raw_data: arp_request.to_binary,
+      actions: SendOutPort.new(interface.port_number),
+    )
   end
 
   def packet_in_icmpv4_echo_request(dpid, message)
     icmp_request = Icmp.read(message.raw_data)
-    send_packet_out(dpid,
-                    raw_data: create_icmp_reply(icmp_request).to_binary,
-                    actions: SendOutPort.new(message.in_port))
+    send_packet_out(
+      dpid,
+      raw_data: create_icmp_reply(icmp_request).to_binary,
+      actions: SendOutPort.new(message.in_port),
+    )
   end
 
   def create_icmp_reply(icmp_request)
-    Icmp::Reply.new(identifier: icmp_request.icmp_identifier,
-                    source_mac: icmp_request.destination_mac,
-                    destination_mac: icmp_request.source_mac,
-                    destination_ip_address: icmp_request.source_ip_address,
-                    source_ip_address: icmp_request.destination_ip_address,
-                    sequence_number: icmp_request.icmp_sequence_number,
-                    echo_data: icmp_request.echo_data)
+    Icmp::Reply.new(
+      identifier: icmp_request.icmp_identifier,
+      source_mac: icmp_request.destination_mac,
+      destination_mac: icmp_request.source_mac,
+      destination_ip_address: icmp_request.source_ip_address,
+      source_ip_address: icmp_request.destination_ip_address,
+      sequence_number: icmp_request.icmp_sequence_number,
+      echo_data: icmp_request.echo_data,
+    )
   end
 
   def add_goto_table_flow_entry(dpid, from_id, to_id, match = Match.new)
@@ -143,16 +153,18 @@ class SimpleRouter < Trema::Controller
   end
 
   def init_classifier_flow_entry(dpid)
-    add_goto_table_flow_entry(dpid,
-                              CLASSIFIER_TABLE_ID,
-                              ROUTING_TABLE_ID,
-                              Match.new(ether_type: ETH_IPv4)
+    add_goto_table_flow_entry(
+      dpid,
+      CLASSIFIER_TABLE_ID,
+      ROUTING_TABLE_ID,
+      Match.new(ether_type: ETH_IPv4),
     )
 
-    add_goto_table_flow_entry(dpid,
-                              CLASSIFIER_TABLE_ID,
-                              ARP_RESPONDER_TABLE_ID,
-                              Match.new(ether_type: ETH_ARP)
+    add_goto_table_flow_entry(
+      dpid,
+      CLASSIFIER_TABLE_ID,
+      ARP_RESPONDER_TABLE_ID,
+      Match.new(ether_type: ETH_ARP),
     )
   end
 
@@ -165,17 +177,18 @@ class SimpleRouter < Trema::Controller
 
   def add_arp_request_flow_entry(dpid, interface)
     instructions = [
+      SendOutPort.new(:controller),
       NiciraRegMove.new(
         from: :source_mac_address,
-        to: :destination_mac_address
+        to: :destination_mac_address,
       ),
       NiciraRegMove.new(
         from: :arp_sender_protocol_address,
-        to: :arp_target_protocol_address
+        to: :arp_target_protocol_address,
       ),
       NiciraRegMove.new(
         from: :arp_sender_hardware_address,
-        to: :arp_target_hardware_address
+        to: :arp_target_hardware_address,
       ),
       SetArpOperation.new(Arp::Reply::OPERATION),
       SetArpSenderHardwareAddress.new(interface.mac_address),
@@ -196,7 +209,7 @@ class SimpleRouter < Trema::Controller
       ),
       instructions: [
         Apply.new(instructions),
-      ]
+      ],
     )
   end
 
@@ -215,7 +228,7 @@ class SimpleRouter < Trema::Controller
         Apply.new([
           SendOutPort.new(:controller)
         ]),
-      ]
+      ],
     )
   end
 
@@ -234,11 +247,11 @@ class SimpleRouter < Trema::Controller
           Apply.new([
             NiciraRegMove.new(
               from: :ipv4_destination_address,
-              to: :reg0
+              to: :reg0,
             ),
           ]),
-	  GotoTable.new(INTERFACE_LOOKUP_TABLE_ID),
-        ]
+          GotoTable.new(INTERFACE_LOOKUP_TABLE_ID),
+        ],
       )
 
       send_flow_mod_add(
@@ -253,7 +266,7 @@ class SimpleRouter < Trema::Controller
           Apply.new([
             SendOutPort.new(:controller)
           ]),
-        ]
+        ],
       )
     end
   end
@@ -276,7 +289,7 @@ class SimpleRouter < Trema::Controller
             ),
             SetSourceMacAddress.new(each.mac_address),
           ]),
-	  GotoTable.new(ARP_LOOKUP_TABLE_ID),
+          GotoTable.new(ARP_LOOKUP_TABLE_ID),
         ]
       )
     end
