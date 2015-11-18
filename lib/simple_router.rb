cb3883456c8a2b4dc58b330f16cd9192bd1ebf2d
yamatchan <yamatchan_yy@yahoo.co.jp>
2015-11-18 15:44:33 +0900

    コード整形。使ってないメソッドも全部削除。rubocopのコメントも削除

--- lib/simple_router.rb
+++ lib/simple_router.rb
@@ -1,9 +1,5 @@
-require 'arp_table'
 require 'interfaces'
-require 'routing_table'
 
-# Simple implementation of L3 switch in OpenFlow1.0
-# rubocop:disable ClassLength
 class SimpleRouter < Trema::Controller
   CLASSIFIER_TABLE_ID       = 0
   ARP_RESPONDER_TABLE_ID    = 2
@@ -18,8 +14,6 @@ class SimpleRouter < Trema::Controller
   def start(_args)
     load File.join(__dir__, '..', 'simple_router.conf')
     @interfaces = Interfaces.new(Configuration::INTERFACES)
-    @arp_table = ArpTable.new
-    @routing_table = RoutingTable.new(Configuration::ROUTES)
     @unresolved_packet_queue = Hash.new { [] }
     logger.info "#{name} started."
   end
@@ -33,7 +27,6 @@ class SimpleRouter < Trema::Controller
     init_packet_out_flow_entry(dpid)
   end
 
-  # rubocop:disable MethodLength
   def packet_in(dpid, message)
     logger.info "-------------"
     logger.info "call packet_in"
@@ -54,12 +47,28 @@ class SimpleRouter < Trema::Controller
       logger.info "Dropping unsupported packet type: #{message.data.inspect}"
     end
   end
-  # rubocop:enable MethodLength 
+
+  private
+  def sent_to_router?(message)
+    return true if message.destination_mac.broadcast?
+    interface = @interfaces.find_by(port_number: message.in_port)
+    interface && interface.mac_address == message.destination_mac
+  end
 
   def packet_in_arp_reply(dpid, message)
     flush_unsent_packets(dpid,
-                         message.data,
-                         @interfaces.find_by(port_number: message.in_port))
+                         message.data.sender_protocol_address,
+                         )
+  end
+
+  def flush_unsent_packets(dpid, destination_ip)
+    @unresolved_packet_queue[destination_ip].each do |each|
+      send_packet_out(dpid,
+        raw_data: each.to_binary_s,
+        actions: SendOutPort.new(:table),
+      )
+    end
+    @unresolved_packet_queue[destination_ip] = []
   end
 
   def packet_in_ipv4(dpid, message)
@@ -75,48 +84,12 @@ class SimpleRouter < Trema::Controller
     end
   end
 
-  # rubocop:disable MethodLength
-  def packet_in_icmpv4_echo_request(dpid, message)
-    icmp_request = Icmp.read(message.raw_data)
-    send_packet_out(dpid,
-                    raw_data: create_icmp_reply(icmp_request).to_binary,
-                    actions: SendOutPort.new(message.in_port))
-  end
-  # rubocop:enable MethodLength
-
-  private
-
-  def sent_to_router?(message)
-    return true if message.destination_mac.broadcast?
-    interface = @interfaces.find_by(port_number: message.in_port)
-    interface && interface.mac_address == message.destination_mac
-  end
-
   def forward?(message)
     !@interfaces.find_by(ip_address: message.destination_ip_address)
   end
 
-  def resolve_next_hop(destination_ip_address)
-    interface = @interfaces.find_by_prefix(destination_ip_address)
-    if interface
-      destination_ip_address
-    else
-      @routing_table.lookup(destination_ip_address)
-    end
-  end
-
-  def create_icmp_reply(icmp_request)
-    Icmp::Reply.new(identifier: icmp_request.icmp_identifier,
-                    source_mac: icmp_request.destination_mac,
-                    destination_mac: icmp_request.source_mac,
-                    destination_ip_address: icmp_request.source_ip_address,
-                    source_ip_address: icmp_request.destination_ip_address,
-                    sequence_number: icmp_request.icmp_sequence_number,
-                    echo_data: icmp_request.echo_data)
-  end
-
   def send_later(dpid, message)
-    destination_ip = resolve_next_hop(message.destination_ip_address)
+    destination_ip = message.destination_ip_address
 
     interface = @interfaces.find_by_prefix(destination_ip)
     return if !interface || (interface.port_number == message.in_port)
@@ -125,15 +98,8 @@ class SimpleRouter < Trema::Controller
     send_arp_request(dpid, destination_ip, interface)
   end
 
-  def flush_unsent_packets(dpid, arp_reply, interface)
-    destination_ip = arp_reply.sender_protocol_address
-    @unresolved_packet_queue[destination_ip].each do |each|
-      send_packet_out(dpid,
-        raw_data: each.to_binary_s,
-        actions: SendOutPort.new(:table)
-      )
-    end
-    @unresolved_packet_queue[destination_ip] = []
+  def resolve_next_hop(destination_ip_address)
+    @interfaces.find_by_prefix(destination_ip_address)
   end
 
   def send_arp_request(dpid, destination_ip, interface)
@@ -147,57 +113,21 @@ class SimpleRouter < Trema::Controller
                     actions: SendOutPort.new(interface.port_number))
   end
 
-  # create add_arp_request_flow_entry by yamatchan
-  def add_arp_request_flow_entry(dpid, message)
-    interface =
-      @interfaces.find_by(port_number: message.in_port,
-                          ip_address: message.data.target_protocol_address)
-    return unless interface
+  def packet_in_icmpv4_echo_request(dpid, message)
+    icmp_request = Icmp.read(message.raw_data)
+    send_packet_out(dpid,
+                    raw_data: create_icmp_reply(icmp_request).to_binary,
+                    actions: SendOutPort.new(message.in_port))
+  end
 
-    # send_flow_mod_add と send_packet_out で使うからまとめとくで
-    instractions = [
-      NiciraRegMove.new(
-        from: :source_mac_address,
-        to: :destination_mac_address
-      ),
-      NiciraRegMove.new(
-        from: :arp_sender_protocol_address,
-        to: :arp_target_protocol_address
-      ),
-      NiciraRegMove.new(
-        from: :arp_sender_hardware_address,
-        to: :arp_target_hardware_address
-      ),
-      SetArpOperation.new(Arp::Reply::OPERATION),
-      #NiciraRegLoad.new(:arp_operation, Arp::Reply::OPERATION),
-      SetArpSenderHardwareAddress.new(interface.mac_address),
-      SetArpSenderProtocolAddress.new(message.data.target_protocol_address),
-      SetSourceMacAddress.new(interface.mac_address),
-      SendOutPort.new((671.to_s(36)+"_"+1198505.to_s(36)).to_sym),
-      #SetEthSrcAddr.new(interface.mac_address),
-    ]
-
-    send_flow_mod_add(
-      dpid,
-      table_id: ARP_RESPONDER_TABLE_ID,
-      priority: 1,
-      match: Match.new(
-        ether_type: ETH_ARP,
-        arp_operation: Arp::Request::OPERATION,
-        arp_target_protocol_address: interface.ip_address,
-      ),
-      instructions: [
-        Apply.new(
-          instractions
-        ),
-      ],
-    )
-
-    send_packet_out(
-      dpid,
-      packet_in: message,
-      actions: instractions
-    )
+  def create_icmp_reply(icmp_request)
+    Icmp::Reply.new(identifier: icmp_request.icmp_identifier,
+                    source_mac: icmp_request.destination_mac,
+                    destination_mac: icmp_request.source_mac,
+                    destination_ip_address: icmp_request.source_ip_address,
+                    source_ip_address: icmp_request.destination_ip_address,
+                    sequence_number: icmp_request.icmp_sequence_number,
+                    echo_data: icmp_request.echo_data)
   end
 
   def add_goto_table_flow_entry(dpid, from_id, to_id)
@@ -289,7 +219,7 @@ class SimpleRouter < Trema::Controller
 
   def init_routing_flow_entry(dpid, interfaces_conf)
     interfaces_conf.map do |each|
-      ip_mask = get_mask(each.fetch(:netmask_length))
+      ip_mask = gen_mask(each.fetch(:netmask_length))
       send_flow_mod_add(
         dpid,
         table_id: ROUTING_TABLE_ID,
@@ -331,7 +261,7 @@ class SimpleRouter < Trema::Controller
 
   def init_interface_lookup_flow_entry(dpid, interfaces_conf)
     interfaces_conf.map do |each|
-      ip_mask = get_mask(each.fetch(:netmask_length))
+      ip_mask = gen_mask(each.fetch(:netmask_length))
       send_flow_mod_add(
         dpid,
         table_id: INTERFACE_LOOKUP_TABLE_ID,
@@ -386,7 +316,6 @@ class SimpleRouter < Trema::Controller
   end
 
   def add_arp_lookup_flow_entry(dpid, message)
-puts message.sender_hardware_address
     send_flow_mod_add(
       dpid,
       table_id: ARP_LOOKUP_TABLE_ID,
@@ -398,14 +327,13 @@ puts message.sender_hardware_address
       instructions: [
         Apply.new([
           SetDestinationMacAddress.new(message.sender_hardware_address),
-#          NiciraRegLoad.new(message.sender_hardware_address, :destination_mac_address),
         ]),
         GotoTable.new(PACKET_OUT_TABLE_ID),
       ]
     )
   end
 
-  def get_mask(len)
+  def gen_mask(len)
     mask = 0
     ((32-len)..31).each{ |p|
       mask |= 1 << p
@@ -413,4 +341,3 @@ puts message.sender_hardware_address
     mask
   end
 end
-# rubocop:enable ClassLength
